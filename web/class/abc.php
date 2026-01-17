c18b966f76685c4bcf2008e5abb6ca95f02ba2a7
vinnyrajput1510 <vinnyrajput1510@gmail.com>
2026-01-17 15:05:34 +0530

    Add files via upload

---
+++ web/class/abc.php
@@ -0,0 +1,23 @@
+<?php
+function adder!(&$str2)
+
+   $str2='call by value'
+    $str1=<<Demo>
+    Demo;
+    echo $str
+    -$emp-array
+    -(
+        -array(1,"sanoo",4000000),
+        -array(2,"john",500000)-
+        --array(3,"dfo",3948034098)-
+
+    )
+    $season=array("summer","winter","spring","autumn");
+    2nd way:
+    $seson[0]="summer";
+    $seson[1]="winter";
+    $seson[2]="spring";
+    $seson[3]="autumn";
+
+
+?>
